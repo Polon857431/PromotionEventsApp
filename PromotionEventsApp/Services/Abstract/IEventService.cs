68ff08220e89a173714312be3508cec99648293a
Karol <karol.karwoski@wp.pl>
2019-04-27 10:19:43 +0200

    Add Event Service

---
+++ PromotionEventsApp/Services/Abstract/IEventService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.ViewModels;
+
+namespace PromotionEventsApp.Services.Abstract
+{
+    public interface IEventService
+    {
+        Task CreateEvent(EventViewModel model);
+        Task<EventViewModel> GetEventViewModel(Guid id);
+        Task UpdateEvent(Guid id, EventViewModel model);
+        Task<List<EventViewModel>> List();
+
+
+    }
+}
---
+++ PromotionEventsApp/Services/EventService.cs
@@ -0,0 +1,42 @@
+﻿using PromotionEventsApp.Models;
+using PromotionEventsApp.Repositories.Abstract;
+using PromotionEventsApp.Services.Abstract;
+using PromotionEventsApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventService(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task CreateEvent(EventViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EventViewModel> GetEventViewModel(Guid id)
+        {
+           
+           return new EventViewModel();
+        }
+
+        public async Task UpdateEvent(Guid id, EventViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<EventViewModel>> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
