6b7ce5dec3ecf5feca38e7da8542400417352a24
Karol <karol.karwoski@wp.pl>
2019-04-27 10:19:04 +0200

    Configuration DB Context

--- PromotionEventsApp/DAL/AppDbContext.cs
+++ PromotionEventsApp/DAL/AppDbContext.cs
@@ -7,6 +7,10 @@ namespace PromotionEventsApp.DAL
     public class AppDbContext : IdentityDbContext<AppUser, AppRole, int>
 
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         private DbSet<Event> Events { get; set; }
     }
 }
--- PromotionEventsApp/Startup.cs
+++ PromotionEventsApp/Startup.cs
@@ -6,9 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using PromotionEventsApp.DAL;
+using PromotionEventsApp.Models;
 using PromotionEventsApp.Repositories;
 using PromotionEventsApp.Repositories.Abstract;
 
@@ -35,10 +40,35 @@ namespace PromotionEventsApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("ConnectionString")));
+
+            services.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                options.ValidationInterval = TimeSpan.Zero;
+            });
+            services.AddIdentity<AppUser, AppRole>(opt =>
+                {
+                    opt.Password.RequiredLength = 6;
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.Password.RequireLowercase = false;
+                    opt.Password.RequireUppercase = false;
+                    opt.Password.RequireDigit = false;
+                    opt.SignIn.RequireConfirmedEmail = false;
+                    opt.User.RequireUniqueEmail = true;
+
+                })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc().AddJsonOptions(x => {
+                x.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+
 
             services.AddScoped<IEventRepository, EventRepository>();
         }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
