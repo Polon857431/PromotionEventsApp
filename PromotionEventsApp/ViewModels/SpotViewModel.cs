f27dcb131d8268888c7c274230a01f7b4c89769d
Karol <karol.karwoski@wp.pl>
2019-05-18 15:39:17 +0200

    add SpotViewModel

---
+++ PromotionEventsApp/ViewModels/SpotViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class SpotViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string QrCode { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public List<Event> Events { get; set; }
+    }
+}
