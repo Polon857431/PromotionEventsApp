9a00c4151b5993a04501e1330ef0ab14822faddc
Karol <karol.karwoski@wp.pl>
2019-05-18 15:46:47 +0200

    Add SpotService Interface

---
+++ PromotionEventsApp/Services/Abstract/ISpotService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+using PromotionEventsApp.ViewModels;
+
+namespace PromotionEventsApp.Services.Abstract
+{
+    public interface ISpotService
+    {
+        Task AddSpot(SpotViewModel model);
+        Task UpdateSpot(SpotViewModel model);
+        Task<List<Spot>> List();
+        Task<List<Spot>> UserSpots(User user);
+        Task<List<Spot>> EventSpots(int eventId);
+        Task EditSpot(SpotViewModel model);
+        Task DeleteSpot(SpotViewModel model);
+    }
+}
