72d023ee2f85fdc8defc7a1f308c4cbb5a4b8c11
Karol <karol.karwoski@wp.pl>
2019-05-18 15:54:32 +0200

    Configuration auto maping svm <-> spot

--- PromotionEventsApp/Profiles/AutoMapperConfig.cs
+++ PromotionEventsApp/Profiles/AutoMapperConfig.cs
@@ -38,6 +38,32 @@ namespace PromotionEventsApp.Profiles
                 .ForMember(_ => _.Spots, __ => __.MapFrom(___ => ___.Spots));
         }
     }
+
+    public class SpotViewModelToSpot : Profile
+    {
+        public SpotViewModelToSpot()
+        {
+            CreateMap<SpotViewModel, Spot>()
+                .ForMember(_ => _.Id, __ => __.MapFrom(___ => ___.Id))
+                .ForMember(_ => _.Description, __ => __.MapFrom(___ => ___.Description))
+                .ForMember(_ => _.Latitude, __ => __.MapFrom(___ => ___.Latitude))
+                .ForMember(_ => _.Longitude, __ => __.MapFrom(___ => ___.Longitude))
+                .ForMember(_ => _.QrCode, __ => __.MapFrom(___ => ___.QrCode));
+        }
+    }
+
+    public class SpotToSpotViewModel : Profile
+    {
+        public SpotToSpotViewModel()
+        {
+            CreateMap<Spot, SpotViewModel>()
+                .ForMember(_ => _.Id, __ => __.MapFrom(___ => ___.Id))
+                .ForMember(_ => _.Description, __ => __.MapFrom(___ => ___.Description))
+                .ForMember(_ => _.Latitude, __ => __.MapFrom(___ => ___.Latitude))
+                .ForMember(_ => _.Longitude, __ => __.MapFrom(___ => ___.Longitude))
+                .ForMember(_ => _.QrCode, __ => __.MapFrom(___ => ___.QrCode));
+        }
+    }
     public class AutoMapperConfig
     {
     }
