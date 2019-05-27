7c3ba61f0fef15e871935de0545911a711ea0382
Karol <karol.karwoski@wp.pl>
2019-05-27 23:42:38 +0200

    Add UserPanel Controller

---
+++ PromotionEventsApp/Controllers/UserPanelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PromotionEventsApp.Models;
+using PromotionEventsApp.Services.Abstract;
+
+namespace PromotionEventsApp.Controllers
+{
+    public class UserPanelController : Controller
+    {
+        private readonly IEventService _eventService;
+        private readonly UserManager<User> _userManager;
+
+        public UserPanelController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public async Task<IActionResult> UserEvents()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var result = await _eventService.UserEvents(user);
+            return View(result);
+        }
+    }
+}
