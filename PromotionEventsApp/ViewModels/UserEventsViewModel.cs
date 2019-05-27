3da5f81f2462f9a1ed251b29f7e197d1614858e9
Karol <karol.karwoski@wp.pl>
2019-05-27 19:17:59 +0200

    Add UserEventViewModel

---
+++ PromotionEventsApp/ViewModels/UserEventsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class UserEventsViewModel
+    {
+        public Event Event { get; set; }
+        public List<Spot> EventSpots { get; set; }
+        public int Points { get; set; }
+    }
+}
