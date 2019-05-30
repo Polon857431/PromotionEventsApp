1384f160dedd9a87bc1dea2e59340479c02dbd51
Karol <karol.karwoski@wp.pl>
2019-05-31 00:20:06 +0200

    code review

--- PromotionEventsApp/Models/EventSpot.cs
+++ PromotionEventsApp/Models/EventSpot.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PromotionEventsApp.Models
+﻿namespace PromotionEventsApp.Models
 {
     public class EventSpot
     {
--- PromotionEventsApp/Models/Member.cs
+++ PromotionEventsApp/Models/Member.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Threading.Tasks;
-
-namespace PromotionEventsApp.Models
+﻿namespace PromotionEventsApp.Models
 {
     public class Member
     {   
