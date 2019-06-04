28ecb2f9c529a7eaa71a1e73e42443b94ad75b4f
Karol <karol.karwoski@wp.pl>
2019-06-05 00:02:49 +0200

    Add migration AddressToEvent

---
+++ PromotionEventsApp/DAL/Migrations/20190604220145_AddresToEvent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PromotionEventsApp.DAL.Migrations
+{
+    public partial class AddresToEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apartment",
+                table: "Events",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Events",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Events",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apartment",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Events");
+        }
+    }
+}
