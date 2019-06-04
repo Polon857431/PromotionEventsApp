11f287a12395a405c33f52302ccbe8a171dcd618
Karol <karol.karwoski@wp.pl>
2019-06-04 16:33:54 +0200

    Create RankingController

---
+++ PromotionEventsApp/Controllers/RankingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromotionEventsApp.Controllers
+{
+    public class RankingController : Controller
+    {
+        private readonly IRankingService _rankingService;
+
+        public RankingController(IRankingService rankingService)
+        {
+            _rankingService = rankingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
