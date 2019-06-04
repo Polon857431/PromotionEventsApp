b922404d33eeca9fbaf1ffe6094b1260875824f8
Karol <karol.karwoski@wp.pl>
2019-06-04 16:34:50 +0200

    Add rank vm

---
+++ PromotionEventsApp/ViewModels/RankViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class RankViewModel
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public int Points { get; set; }
+    }
+}
