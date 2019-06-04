79f0777f0e12d1ba59123c9ce943ac6bfc0b69ee
Karol <karol.karwoski@wp.pl>
2019-06-04 16:34:13 +0200

    Add rank repositery

---
+++ PromotionEventsApp/Repositories/Abstract/IRankRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.Repositories.Abstract
+{
+    public  interface IRankRepository
+    {
+        Task<IEnumerable<VisitedSpot>> GetAllAsync();
+        Task<IEnumerable<VisitedSpot>> GetAllAsync(params Expression<Func<VisitedSpot, object>>[] includeProperties);
+    }
+}
---
+++ PromotionEventsApp/Repositories/RankRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PromotionEventsApp.DAL;
+using PromotionEventsApp.Models;
+using PromotionEventsApp.Repositories.Abstract;
+
+namespace PromotionEventsApp.Repositories
+{
+    public class RankRepository : IRankRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RankRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private IQueryable<VisitedSpot> Includer(IQueryable<VisitedSpot> query,
+            params Expression<Func<VisitedSpot, object>>[] includeProperties)
+        {
+            foreach (var includeProperty in includeProperties)
+                query = query.Include(includeProperty);
+            return query;
+        }
+
+
+
+
+        public async Task<IEnumerable<VisitedSpot>> GetAllAsync()
+        {
+            IQueryable<VisitedSpot> query = _context.Set<VisitedSpot>();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<VisitedSpot>> GetAllAsync(params Expression<Func<VisitedSpot, object>>[] includeProperties)
+        {
+            IQueryable<VisitedSpot> query = _context.Set<VisitedSpot>();
+            query = Includer(query, includeProperties);
+            return await query.ToListAsync();
+        }
+    }
+}
