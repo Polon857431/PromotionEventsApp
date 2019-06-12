1e0fc06e3a0ca8ee81a4a915c8c0bef3364e09c1
Karol <karol.karwoski@wp.pl>
2019-06-12 11:57:02 +0200

    Add new methods in UserPanel controller

--- PromotionEventsApp/Controllers/UserPanelController.cs
+++ PromotionEventsApp/Controllers/UserPanelController.cs
@@ -23,5 +23,31 @@ namespace PromotionEventsApp.Controllers
             var result = await _eventService.UserEvents(user);
             return View(result);
         }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _userManager.FindByNameAsync(User.Identity.Name));
+        }
+
+        public IActionResult ChangePersonalData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult ChangeEmail()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult ChangePassword()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult UserRank()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
