99247a5eaf5eb63c4ec7549fd0abd4aafe514382
Karol <karol.karwoski@wp.pl>
2019-06-12 13:41:23 +0200

    Add UserService

---
+++ PromotionEventsApp/Services/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PromotionEventsApp.Models;
+using PromotionEventsApp.ViewModels;
+
+namespace PromotionEventsApp.Services.Abstract
+{
+    public interface IUserService
+    {
+        Task ChangePersonalData(UserPersonalDataViewModel model, User user);
+        Task ChangePassword(ChangePasswordViewModel model, User user);
+        Task ChangeEmail(ChangeEmailViewModel model, User user);
+
+    }
+}
---
+++ PromotionEventsApp/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PromotionEventsApp.Models;
+using PromotionEventsApp.Services.Abstract;
+using PromotionEventsApp.ViewModels;
+
+namespace PromotionEventsApp.Services
+{
+    public class UserService: IUserService
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public async Task ChangePersonalData(UserPersonalDataViewModel model, User user)
+        {
+           await _userManager.UpdateAsync(user);
+        }
+
+        public async Task ChangePassword(ChangePasswordViewModel model, User user)
+        {
+            await _userManager.UpdateAsync(user);
+
+        }
+
+        public async Task ChangeEmail(ChangeEmailViewModel model, User user)
+        {
+            await _userManager.UpdateAsync(user);
+
+        }
+    }
+}
