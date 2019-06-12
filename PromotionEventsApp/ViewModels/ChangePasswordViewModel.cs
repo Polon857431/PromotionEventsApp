b5b7235324414da5843488d368548c00b966a1ad
Karol <karol.karwoski@wp.pl>
2019-06-12 12:05:17 +0200

    Add ChangePasswordVm

---
+++ PromotionEventsApp/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string OldPassword { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
