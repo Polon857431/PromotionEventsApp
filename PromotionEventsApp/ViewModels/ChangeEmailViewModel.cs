a6348d8b4a333f40be4a5e8393430b095c2d8700
Karol <karol.karwoski@wp.pl>
2019-06-12 12:07:00 +0200

    Add ChangeEmailViewModel

---
+++ PromotionEventsApp/ViewModels/ChangeEmailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class ChangeEmailViewModel
+    {
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string NewEmail { get; set; }
+        public string NewEmailConfirm { get; set; }
+
+    }
+}
