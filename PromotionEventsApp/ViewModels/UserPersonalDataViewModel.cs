efa14dbeb0169d2bd4a35ae62551afd8a635178b
Karol <karol.karwoski@wp.pl>
2019-06-12 12:03:08 +0200

    Add ChangePersonalData vm

---
+++ PromotionEventsApp/ViewModels/UserPersonalDataViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.ViewModels
+{
+    public class UserPersonalDataViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Local { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
+
