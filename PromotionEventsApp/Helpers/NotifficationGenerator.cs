fb0373193d0e8e48b3732461b1c3b14bf82cf823
Karol <karol.karwoski@wp.pl>
2019-06-17 08:40:13 +0200

    Add NotificationGenerator

---
+++ PromotionEventsApp/Helpers/NotifficationGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.Helpers
+{
+    public class NotificationGenerator
+    {
+        public static Notification CreateNotification(string title, string content, string color, string icon)
+        {
+            return new Notification()
+            {
+                Title = title,
+                Content = content,
+                Color = color,
+                Icon = icon
+            };
+        }
+    }
+}
