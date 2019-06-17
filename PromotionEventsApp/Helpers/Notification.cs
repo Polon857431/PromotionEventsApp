d4e9586bef7387cdc0e94a2cac8c2efe4730c378
Karol <karol.karwoski@wp.pl>
2019-06-17 08:39:59 +0200

    Add Notification

---
+++ PromotionEventsApp/Helpers/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionEventsApp.Helpers
+{
+    public class Notification
+    {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public string Content { get; set; }
+
+    }
+}
