a1a114abd787feda7c2477340c136c783e62af6c
karol.karwoski <karol.karwoski@gmail.com>
2019-07-16 10:04:34 +0200

    Fix user panel index method

--- PromotionEventsApp/Extensions/ContextAccessorExtensions.cs
+++ PromotionEventsApp/Extensions/ContextAccessorExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using PromotionEventsApp.Models;
 
 namespace PromotionEventsApp.Extensions
 {
@@ -14,5 +15,6 @@ namespace PromotionEventsApp.Extensions
             int.TryParse(accessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value, out var result);
             return result;
         }
+
     }
 }
