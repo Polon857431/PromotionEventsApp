1137e0fd510aa0d818ec6cf2a71e13433181622a
karol.karwoski <karol.karwoski@gmail.com>
2019-07-16 09:46:30 +0200

    Add ContextAccessorExtension

---
+++ PromotionEventsApp/Extensions/ContextAccessorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PromotionEventsApp.Extensions
+{
+    public static class ContextAccessorExtensions
+    {
+   
+    }
+}
