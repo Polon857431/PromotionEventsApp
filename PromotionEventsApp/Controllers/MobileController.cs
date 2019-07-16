260a4eaa29aae19da2b2cbaddddc4db2271c79ef
Karol <karol.karwoski@wp.pl>
2019-07-16 20:14:45 +0200

    Add MobileController

---
+++ PromotionEventsApp/Controllers/MobileController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromotionEventsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobileController : ControllerBase
+    {
+        // GET: api/Mobile
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Mobile/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Mobile
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Mobile/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
