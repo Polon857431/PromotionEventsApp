041d05eed36d0fbad4db6c462e930b0cec55509f
karol.karwoski <karol.karwoski@gmail.com>
2019-07-19 15:49:18 +0200

    Add User Initiazer

---
+++ PromotionEventsApp/DAL/Initializers/UserInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.DAL.Initializers
+{
+    public class UserInitializer
+    {
+        public static async Task CreateAdminAccount(UserManager<User> userManager, RoleManager<Role> roleManager, IConfiguration configuration)
+        {
+            var superAdmin = new User()
+            {
+                UserName = configuration["SuperAdmin:UserName"],
+                FirstName = configuration["SuperAdmin:FirstName"],
+                LastName = configuration["SuperAdmin:LastName"],
+                Email = configuration["SuperAdmin:Email"],
+                EmailConfirmed = true
+            };
+            if (await userManager.FindByEmailAsync(superAdmin.Email) == null)
+            {
+                await userManager.CreateAsync(superAdmin, configuration["SuperAdmin:Password"]);
+                await userManager.AddToRolesAsync(superAdmin, roleManager.Roles.Select(_ => _.Name).ToList());
+            }
+        }
+
+        public static async Task CreateTestAccount(UserManager<User> userManager, RoleManager<Role> roleManager, IConfiguration configuration)
+        {
+            var testUser = new User()
+            {
+                UserName = configuration["TestUser:UserName"],
+                FirstName = configuration["TestUser:FirstName"],
+                LastName = configuration["TestUser:LastName"],
+                Email = configuration["TestUser:Email"],
+                EmailConfirmed = true
+            };
+            if (await userManager.FindByEmailAsync(testUser.Email) == null)
+            {
+                await userManager.CreateAsync(testUser, configuration["TestUser:Password"]);
+                await userManager.AddToRoleAsync(testUser, "User");
+            }
+        }
+    }
+}
+}
