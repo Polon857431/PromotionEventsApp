9accee66550cac3d02923a01c828fff67b035689
karol.karwoski <karol.karwoski@gmail.com>
2019-07-19 15:31:04 +0200

    Add RoleInitializer

---
+++ PromotionEventsApp/DAL/Initializers/RoleInitalizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.DAL.Initializers
+{
+    public class RoleInitalizer
+    {
+        private static readonly List<string> Roles = new List<string>()
+        {
+            "Admin",
+            "User"
+        };
+
+        public static async Task Initalize(RoleManager<Role> roleManager)
+        {
+            foreach (var role in Roles)
+            {
+                var roleInDb = await roleManager.FindByNameAsync(role);
+                if (roleInDb == null)
+                {
+                    await roleManager.CreateAsync(new Role() { Name = role });
+                }
+          
+            }
+        }
+    }
+}
