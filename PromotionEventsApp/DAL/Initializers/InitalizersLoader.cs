67763cf93ad114cfe9dc0f359555bc2fba7897e7
Karol <karol.karwoski@wp.pl>
2019-07-20 09:41:09 +0200

    Add InitalizersLoader

---
+++ PromotionEventsApp/DAL/Initializers/InitalizersLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using PromotionEventsApp.Models;
+
+namespace PromotionEventsApp.DAL.Initializers
+{
+    public static class InitalizersLoader
+    {
+        public static async Task LoadInitializers(RoleManager<Role> roleManager, UserManager<User> userManager , IConfiguration configuration)
+        {
+           await RoleInitalizer.Initalize(roleManager);
+           await UserInitializer.CreateAdminAccount(userManager, roleManager, configuration);
+           await UserInitializer.CreateTestAccount(userManager, roleManager, configuration);
+        }
+    }
+}
